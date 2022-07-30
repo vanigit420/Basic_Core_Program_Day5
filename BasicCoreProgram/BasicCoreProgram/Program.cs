1e85d8950cf3a552da4d17a9b02df32a13fdddc4
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-30 11:06:38 +0530

    Computing question and remainder

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -4,34 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the 1st number : ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Dividend : ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Divisor : ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Enter the 2nd number : ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            int Quotient = dividend / divisor;
+            int Remainder = dividend % divisor;
 
-            Console.WriteLine("Enter the 3rd number : ");
-            int num3 = Convert.ToInt32(Console.ReadLine());
-
-            if (num1 > num2)
-            {
-                if (num1 > num3)
-                {
-                    Console.WriteLine(num1 + " num1 is greatest among three numbers");
-                }
-                else if (num3 > num2)
-                {
-                    Console.WriteLine(num3 + " num3 is greatest among three numbers");
-                }
-            }
-            else if (num2 > num3)
-            {
-                Console.WriteLine(num2 + " num2 is greatest among three numbers");
-            }
-            else
-            {
-                Console.WriteLine("Numbers are Equals");
-            }
+            Console.WriteLine("Dividend: {0}, Divisor: {1} ", dividend, divisor);
+            Console.WriteLine("Quotient is : " + Quotient);
+            Console.WriteLine("Remainder is : " + Remainder);
         }
     }
 }
