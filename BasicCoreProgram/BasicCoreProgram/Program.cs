c38abdef12bc3a949369ad77a246d71e7c8c5faa
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-30 10:49:05 +0530

    finding the factors of number

---
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the Number : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Factors are : ");
+
+            if (num >= 2)
+            {
+                for (int i = 2; i <= num; i++)
+                {
+                    while (num % i == 0)
+                    {
+                        Console.Write(i + " ");
+                        num = num / i;
+                    }
+                }
+            }
+        }
+    }
+}
