cbb5f61ffbe972f8b4b046dee727ced06d90ca27
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-30 11:03:41 +0530

    find the largest number among three numbers

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -4,22 +4,33 @@
     {
         static void Main(string[] args)
         {
-            int num;
-            Console.WriteLine("Enter the Number : ");
-            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 1st number : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Factors are : ");
+            Console.WriteLine("Enter the 2nd number : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
 
-            if (num >= 2)
+            Console.WriteLine("Enter the 3rd number : ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            if (num1 > num2)
             {
-                for (int i = 2; i <= num; i++)
+                if (num1 > num3)
                 {
-                    while (num % i == 0)
-                    {
-                        Console.Write(i + " ");
-                        num = num / i;
-                    }
+                    Console.WriteLine(num1 + " num1 is greatest among three numbers");
                 }
+                else if (num3 > num2)
+                {
+                    Console.WriteLine(num3 + " num3 is greatest among three numbers");
+                }
+            }
+            else if (num2 > num3)
+            {
+                Console.WriteLine(num2 + " num2 is greatest among three numbers");
+            }
+            else
+            {
+                Console.WriteLine("Numbers are Equals");
             }
         }
     }
