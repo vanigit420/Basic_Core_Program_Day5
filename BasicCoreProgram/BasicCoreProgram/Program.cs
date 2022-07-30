453f28cf9595044f63b806eb3cdcf40d8d5e8afd
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-30 10:51:53 +0530

    printing percentage of head and tails

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -4,22 +4,35 @@
     {
         static void Main(string[] args)
         {
-            int num;
-            Console.WriteLine("Enter the Number : ");
-            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Number of times Flip The Coin : ");
+            int FlipCoin = Convert.ToInt32(Console.ReadLine());
+            int Head = 0;
+            int Tail = 0;
+            int HeadCount = 0;
+            int TailCount = 0;
+            int HeadPercentage;
+            int TailPercentage;
 
-            Console.WriteLine("Factors are : ");
-
-            if (num >= 2)
+            if (FlipCoin > 0)
             {
-                for (int i = 2; i <= num; i++)
+                for (int i = 1; i <= FlipCoin; i++)
                 {
-                    while (num % i == 0)
+                    Random random = new Random();
+                    int num = random.Next(0, 2);
+                    if (num == 1)
                     {
-                        Console.Write(i + " ");
-                        num = num / i;
+                        HeadCount++;
                     }
+                    else
+                    {
+                        TailCount++;
+                    }
+                    Console.WriteLine(num);
                 }
+                HeadPercentage = HeadCount * 100 / FlipCoin;
+                TailPercentage = TailCount * 100 / FlipCoin;
+                Console.WriteLine("HeadPercentage is : " + HeadPercentage);
+                Console.WriteLine("TailPercentage is : " + TailPercentage);
             }
         }
     }
