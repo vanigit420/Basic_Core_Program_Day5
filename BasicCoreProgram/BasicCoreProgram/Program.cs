c8c3f73d232a323d0f34138849a50b695ba2ef63
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-30 10:54:58 +0530

    printing Harmonic number series

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -4,36 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Number of times Flip The Coin : ");
-            int FlipCoin = Convert.ToInt32(Console.ReadLine());
-            int Head = 0;
-            int Tail = 0;
-            int HeadCount = 0;
-            int TailCount = 0;
-            int HeadPercentage;
-            int TailPercentage;
+            int num;
+            double sum = 0;
+            Console.WriteLine("Calculate the Harmonic Series and their sum :\n");
+            Console.WriteLine("-----------------");
 
-            if (FlipCoin > 0)
+            Console.WriteLine("Input the Number of terms : ");
+            num = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= num; i++)
             {
-                for (int i = 1; i <= FlipCoin; i++)
-                {
-                    Random random = new Random();
-                    int num = random.Next(0, 2);
-                    if (num == 1)
-                    {
-                        HeadCount++;
-                    }
-                    else
-                    {
-                        TailCount++;
-                    }
-                    Console.WriteLine(num);
-                }
-                HeadPercentage = HeadCount * 100 / FlipCoin;
-                TailPercentage = TailCount * 100 / FlipCoin;
-                Console.WriteLine("HeadPercentage is : " + HeadPercentage);
-                Console.WriteLine("TailPercentage is : " + TailPercentage);
+                Console.Write(" 1/{0} + ", i);
+                sum += (double)1 / i;
             }
+            Console.WriteLine("\nSum of series upto {0} terms : {1} \n", num, sum);
         }
     }
 }
