17d08766c08683cf22395fbff9c3ddeee0279622
shivani korwan <shivanikorwan63232@gmail.com>
2022-07-30 10:57:50 +0530

    printing year leap year or ot

--- BasicCoreProgram/BasicCoreProgram/Program.cs
+++ BasicCoreProgram/BasicCoreProgram/Program.cs
@@ -4,20 +4,19 @@
     {
         static void Main(string[] args)
         {
-            int num;
-            double sum = 0;
-            Console.WriteLine("Calculate the Harmonic Series and their sum :\n");
-            Console.WriteLine("-----------------");
+            int year;
+            Console.WriteLine("Enter the Four Digit Number Year : ");
+            year = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Input the Number of terms : ");
-            num = Convert.ToInt32(Console.ReadLine());
-
-            for (int i = 1; i <= num; i++)
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
             {
-                Console.Write(" 1/{0} + ", i);
-                sum += (double)1 / i;
+                Console.WriteLine("{0} year is Leap Year", year);
             }
-            Console.WriteLine("\nSum of series upto {0} terms : {1} \n", num, sum);
+            else
+            {
+                Console.WriteLine("{0} year is not Leap Year", year);
+            }
+            Console.ReadLine();
         }
     }
 }
